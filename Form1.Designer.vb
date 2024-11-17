dc8601ff96ebdcc3661466cdbff42bd8eebe2dcb
Utilisateur <Utilisateur@DESKTOP-A2DOGB0>
2024-11-17 19:21:11 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Txt_affiche = New TextBox()
+        Btn_0 = New Button()
+        Btn_9 = New Button()
+        Btn_8 = New Button()
+        Btn_7 = New Button()
+        Btn_6 = New Button()
+        Btn_5 = New Button()
+        Btn_4 = New Button()
+        Btn_3 = New Button()
+        Btn_2 = New Button()
+        Btn_1 = New Button()
+        Btn_div = New Button()
+        Btn_mult = New Button()
+        Btn_moins = New Button()
+        Btn_plus = New Button()
+        Btn_result = New Button()
+        Btn_C = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Txt_affiche
+        ' 
+        Txt_affiche.Font = New Font("MS Reference Sans Serif", 9F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Txt_affiche.ForeColor = Color.SaddleBrown
+        Txt_affiche.Location = New Point(136, 22)
+        Txt_affiche.Name = "Txt_affiche"
+        Txt_affiche.Size = New Size(244, 22)
+        Txt_affiche.TabIndex = 0
+        ' 
+        ' Btn_0
+        ' 
+        Btn_0.BackColor = SystemColors.ActiveCaption
+        Btn_0.FlatStyle = FlatStyle.Flat
+        Btn_0.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_0.ForeColor = SystemColors.ControlText
+        Btn_0.Location = New Point(155, 60)
+        Btn_0.Name = "Btn_0"
+        Btn_0.Size = New Size(33, 23)
+        Btn_0.TabIndex = 1
+        Btn_0.Text = "0"
+        Btn_0.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_9
+        ' 
+        Btn_9.BackColor = SystemColors.ActiveCaption
+        Btn_9.FlatStyle = FlatStyle.Flat
+        Btn_9.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Btn_9.Location = New Point(210, 146)
+        Btn_9.Name = "Btn_9"
+        Btn_9.Size = New Size(33, 23)
+        Btn_9.TabIndex = 2
+        Btn_9.Text = "9"
+        Btn_9.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_8
+        ' 
+        Btn_8.BackColor = SystemColors.ActiveCaption
+        Btn_8.FlatStyle = FlatStyle.Flat
+        Btn_8.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Btn_8.Location = New Point(155, 146)
+        Btn_8.Name = "Btn_8"
+        Btn_8.Size = New Size(33, 23)
+        Btn_8.TabIndex = 3
+        Btn_8.Text = "8"
+        Btn_8.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_7
+        ' 
+        Btn_7.BackColor = SystemColors.ActiveCaption
+        Btn_7.FlatStyle = FlatStyle.Flat
+        Btn_7.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_7.Location = New Point(323, 103)
+        Btn_7.Name = "Btn_7"
+        Btn_7.RightToLeft = RightToLeft.No
+        Btn_7.Size = New Size(33, 23)
+        Btn_7.TabIndex = 4
+        Btn_7.Text = "7"
+        Btn_7.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_6
+        ' 
+        Btn_6.BackColor = SystemColors.ActiveCaption
+        Btn_6.FlatStyle = FlatStyle.Flat
+        Btn_6.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_6.Location = New Point(267, 103)
+        Btn_6.Name = "Btn_6"
+        Btn_6.Size = New Size(33, 23)
+        Btn_6.TabIndex = 5
+        Btn_6.Text = "6"
+        Btn_6.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_5
+        ' 
+        Btn_5.BackColor = SystemColors.ActiveCaption
+        Btn_5.FlatStyle = FlatStyle.Flat
+        Btn_5.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_5.Location = New Point(210, 103)
+        Btn_5.Name = "Btn_5"
+        Btn_5.Size = New Size(33, 23)
+        Btn_5.TabIndex = 6
+        Btn_5.Text = "5"
+        Btn_5.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_4
+        ' 
+        Btn_4.BackColor = SystemColors.ActiveCaption
+        Btn_4.FlatStyle = FlatStyle.Flat
+        Btn_4.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_4.Location = New Point(155, 103)
+        Btn_4.Name = "Btn_4"
+        Btn_4.Size = New Size(33, 23)
+        Btn_4.TabIndex = 7
+        Btn_4.Text = "4"
+        Btn_4.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_3
+        ' 
+        Btn_3.BackColor = SystemColors.ActiveCaption
+        Btn_3.FlatStyle = FlatStyle.Flat
+        Btn_3.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_3.Location = New Point(323, 60)
+        Btn_3.Name = "Btn_3"
+        Btn_3.Size = New Size(33, 23)
+        Btn_3.TabIndex = 8
+        Btn_3.Text = "3"
+        Btn_3.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_2
+        ' 
+        Btn_2.BackColor = SystemColors.ActiveCaption
+        Btn_2.FlatStyle = FlatStyle.Flat
+        Btn_2.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_2.Location = New Point(267, 60)
+        Btn_2.Name = "Btn_2"
+        Btn_2.Size = New Size(33, 23)
+        Btn_2.TabIndex = 9
+        Btn_2.Text = "2"
+        Btn_2.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_1
+        ' 
+        Btn_1.BackColor = SystemColors.ActiveCaption
+        Btn_1.FlatStyle = FlatStyle.Flat
+        Btn_1.Font = New Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold)
+        Btn_1.Location = New Point(210, 60)
+        Btn_1.Name = "Btn_1"
+        Btn_1.Size = New Size(33, 23)
+        Btn_1.TabIndex = 10
+        Btn_1.Text = "1"
+        Btn_1.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_div
+        ' 
+        Btn_div.BackColor = Color.Chocolate
+        Btn_div.FlatStyle = FlatStyle.Flat
+        Btn_div.Font = New Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold)
+        Btn_div.Location = New Point(323, 186)
+        Btn_div.Name = "Btn_div"
+        Btn_div.Size = New Size(33, 23)
+        Btn_div.TabIndex = 11
+        Btn_div.Text = "/"
+        Btn_div.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_mult
+        ' 
+        Btn_mult.BackColor = Color.Chocolate
+        Btn_mult.FlatStyle = FlatStyle.Flat
+        Btn_mult.Font = New Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold)
+        Btn_mult.Location = New Point(267, 186)
+        Btn_mult.Name = "Btn_mult"
+        Btn_mult.Size = New Size(33, 23)
+        Btn_mult.TabIndex = 12
+        Btn_mult.Text = "*"
+        Btn_mult.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_moins
+        ' 
+        Btn_moins.BackColor = Color.Chocolate
+        Btn_moins.FlatStyle = FlatStyle.Flat
+        Btn_moins.Font = New Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold)
+        Btn_moins.Location = New Point(210, 186)
+        Btn_moins.Name = "Btn_moins"
+        Btn_moins.Size = New Size(33, 23)
+        Btn_moins.TabIndex = 13
+        Btn_moins.Text = "-"
+        Btn_moins.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_plus
+        ' 
+        Btn_plus.BackColor = Color.Chocolate
+        Btn_plus.FlatStyle = FlatStyle.Flat
+        Btn_plus.Font = New Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold)
+        Btn_plus.Location = New Point(155, 186)
+        Btn_plus.Name = "Btn_plus"
+        Btn_plus.Size = New Size(33, 23)
+        Btn_plus.TabIndex = 14
+        Btn_plus.Text = "+"
+        Btn_plus.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_result
+        ' 
+        Btn_result.BackColor = Color.Chocolate
+        Btn_result.FlatStyle = FlatStyle.Flat
+        Btn_result.Font = New Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn_result.Location = New Point(323, 146)
+        Btn_result.Name = "Btn_result"
+        Btn_result.Size = New Size(33, 23)
+        Btn_result.TabIndex = 15
+        Btn_result.Text = "="
+        Btn_result.UseVisualStyleBackColor = False
+        ' 
+        ' Btn_C
+        ' 
+        Btn_C.BackColor = SystemColors.Highlight
+        Btn_C.FlatStyle = FlatStyle.Flat
+        Btn_C.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Bold)
+        Btn_C.Location = New Point(267, 146)
+        Btn_C.Name = "Btn_C"
+        Btn_C.Size = New Size(33, 23)
+        Btn_C.TabIndex = 16
+        Btn_C.Text = "C"
+        Btn_C.UseVisualStyleBackColor = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Txt_affiche)
+        GroupBox1.Controls.Add(Btn_div)
+        GroupBox1.Controls.Add(Btn_mult)
+        GroupBox1.Controls.Add(Btn_moins)
+        GroupBox1.Controls.Add(Btn_plus)
+        GroupBox1.Controls.Add(Btn_result)
+        GroupBox1.Controls.Add(Btn_C)
+        GroupBox1.Controls.Add(Btn_0)
+        GroupBox1.Controls.Add(Btn_1)
+        GroupBox1.Controls.Add(Btn_2)
+        GroupBox1.Controls.Add(Btn_3)
+        GroupBox1.Controls.Add(Btn_4)
+        GroupBox1.Controls.Add(Btn_5)
+        GroupBox1.Controls.Add(Btn_9)
+        GroupBox1.Controls.Add(Btn_8)
+        GroupBox1.Controls.Add(Btn_7)
+        GroupBox1.Controls.Add(Btn_6)
+        GroupBox1.Location = New Point(123, 53)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(517, 261)
+        GroupBox1.TabIndex = 17
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Calculatrice 02"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Txt_affiche As TextBox
+    Friend WithEvents Btn_0 As Button
+    Friend WithEvents Btn_9 As Button
+    Friend WithEvents Btn_8 As Button
+    Friend WithEvents Btn_7 As Button
+    Friend WithEvents Btn_6 As Button
+    Friend WithEvents Btn_5 As Button
+    Friend WithEvents Btn_4 As Button
+    Friend WithEvents Btn_3 As Button
+    Friend WithEvents Btn_2 As Button
+    Friend WithEvents Btn_1 As Button
+    Friend WithEvents Btn_div As Button
+    Friend WithEvents Btn_mult As Button
+    Friend WithEvents Btn_moins As Button
+    Friend WithEvents Btn_plus As Button
+    Friend WithEvents Btn_result As Button
+    Friend WithEvents Btn_C As Button
+    Friend WithEvents GroupBox1 As GroupBox
+
+End Class
